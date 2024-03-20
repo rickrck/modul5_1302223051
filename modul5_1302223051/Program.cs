b1932eb88ee5710bff6367abd7a2e7ee9e0a8c7a
unknown <rickyrenaldi100@gmail.com>
2024-03-20 07:41:56 +0700

    JumlahTigaAngka

--- modul5_1302223051/Program.cs
+++ modul5_1302223051/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Penjumlahan<float> jumlah = new Penjumlahan<float>();
+
+        jumlah.Jumlah<float>(22, 30, 51);
+
+    }
+
+}
+
+public class Penjumlahan<T>
+{
+
+    public void Jumlah<T>(T angka1, T angka2, T angka3)
+    {
+        dynamic jumlah1 = angka1;
+        dynamic jumlah2 = angka2;
+        dynamic jumlah3 = angka3;
+
+        dynamic total = jumlah1 + jumlah2 + jumlah3;
+
+        Console.WriteLine(total);
+    }
+}
